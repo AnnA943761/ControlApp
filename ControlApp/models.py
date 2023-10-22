e84c953bf00cde2a7d8f240559cfdf42d15b33b1
AnnA943761 <119124836+AnnA943761@users.noreply.github.com>
2023-10-22 23:25:42 +0700

    first commit

---
+++ ControlApp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(verbose_name="Заголовок", max_length=50, blank=False)
+    content = models.TextField(verbose_name="Содержание", blank=False)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
